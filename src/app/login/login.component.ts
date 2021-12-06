428f91d1b8c810ab8f72a97c4cea738bb84a82bb
Asaf <asaf1802@gmail.com>
2021-12-06 20:06:53 +0200

    check11

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../auth.service';
+
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  
+  
+  email!:string;
+  password!:string;
 
-  constructor() { }
+  onSubmit(){
+    this.auth.login(this.email,this.password);
+  }
+  
+  constructor(private auth:AuthService ) { }
 
   ngOnInit(): void {
   }
